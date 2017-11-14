50f6bec24ee08e1e3b72a7c5be14c5be0f7e9155
Arian <arianmarkus77@yahoo.com>
2017-11-14 22:53:05 +0700

    Create post page

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,65 +1,141 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
 import {
   Row,
+  Container,
   Col,
   Card,
-  CardText,
   CardBody,
-  CardTitle,
-  CardSubtitle,
-  Button
-} from "reactstrap"
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
-import DATA_QUESTIONS from "../data/questions"
+// STYLING_CSS ---------------------------------------------------
 
-const QuestionCard = props => (
-  <Card>
+const styleForm = {
+  width: "50%",
+  height: "650px"
+};
+
+const styleButton = {
+  width: "100%",
+  bottom: "0"
+};
+
+const styleText = {
+  fontFamily: "lato",
+  width: "60%",
+  fontSize: "16px",
+  marginRight: "100px"
+};
+
+const styleHeader = {
+  fontFamily: "lato",
+  fontSize: "16px",
+  margin: "0px",
+  fontWeight: "bold",
+  borderBottom: "solid #1DE9B6",
+  marginRight: "240px"
+};
+
+const styleTextarea = {
+  width: "500px",
+  height: "100px"
+};
+
+// COLUMN_POST
+
+const ColumnPost = () => (
+  <Card style={styleText}>
     <CardBody>
-      <CardTitle>{props.title}</CardTitle>
-      <CardSubtitle>
-        Asked by <Link to={`/profile/${props.userId}`}>{props.userName}</Link>
-      </CardSubtitle>
-      <CardText>
-        <Button color="success" size="sm">
-          Answer
+      <h3 style={styleHeader}>REGISTER</h3>
+      <Form style={styleForm}>
+        <FormGroup>
+          <Label for="eventTitle" />
+          <Input
+            type="text"
+            name="eventTitle"
+            id="eventTitle"
+            placeholder="Event Title"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="eventDate" />
+          <Input
+            type="Date"
+            name="eventDate"
+            id="eventDate"
+            placeholder="Event Date"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="eventVenue" />
+          <Input
+            type="text"
+            name="eventVenue"
+            id="eventVenue"
+            placeholder="Venue"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="eventTime" />
+          <Input
+            type="time"
+            name="eventTime"
+            id="eventTime"
+            placeholder="Event Time"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="eventCategory" />
+          <Input
+            type="text"
+            name="eventCategory"
+            id="eventCategory"
+            placeholder="Event Category"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="upload" />
+          <Input
+            type="file"
+            name="picture"
+            id="picture"
+            accept="image/*"
+            placeholder="Upload Picture"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="EventDescription" />
+          <textArea
+            style={styleTextarea}
+            type="text"
+            name="EventDescription"
+            id="EventDescription"
+            placeholder="Event Description"
+          />
+        </FormGroup>
+        <Button style={styleButton} color="success">
+          PUBLISH
         </Button>
-      </CardText>
+      </Form>
     </CardBody>
   </Card>
-)
+);
 
-const Question = props => {
-  const question = DATA_QUESTIONS.filter(question => {
-    return question._id === Number(props.match.params.id)
-  })
-  const answers = question[0].answers
+// POST
 
-  return (
-    <Col>
-      <QuestionCard
-        title={question[0].title}
-        userId={question[0].createdBy._id}
-        userName={question[0].createdBy.name}
-      />
+const Post = () => (
+  <Container>
+    <Row className="center" id="row-auth">
+      <Col>
+        <ColumnPost />
+      </Col>
+    </Row>
+  </Container>
+);
 
-      <div>
-        {answers.length ? `${answers.length} answers` : `No answer yet`}
-      </div>
-
-      <hr />
-
-      {answers.map(answer => {
-        return (
-          <div>
-            <h6>{answer.createdBy.name}</h6>
-            <p>{answer.text}</p>
-            <hr />
-          </div>
-        )
-      })}
-    </Col>
-  )
-}
-
-export default Question
+export default Post;
